bd37edd8f6c5a401a4a5418ed10782b2331961f7
Leo <e23882@hotmail.com>
2019-04-19 17:51:29 +0800

    Oracle9iManagerStudio

---
+++ OracleManagerStudio/Login.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CommonFunction;
+
+namespace OracleManagerStudio
+{
+    /// <summary>
+    /// Login.xaml 的互動邏輯
+    /// </summary>
+    public partial class Login : Window
+    {
+        #region Declarations
+        public string dbName { get; set; }
+        public string Status { get; set; }
+
+        public string IP { get; set; }
+        public List<string> allTable { get; set; }
+        #endregion
+
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtConnect_Click(object sender, RoutedEventArgs e)
+        {
+            string query = string.Empty;
+            int count = 0;
+            //original connection string "Provider=MSDAORA.1;Data Source=OFD.world;User ID=sw;Password=ofdadm1116;"
+            OraConnection ofd = new OraConnection(tbDBIP.Text, tbDBName.Text, tbID.Text, tbPW.Text);
+            var dtofd = ofd.ExecQuery("select count(*) from v$session;");
+            foreach (DataRow dr in dtofd.Rows)
+            {
+                count++;
+            }
+            if (count > 0)
+            {
+                dbName = tbDBName.Text;
+                Status = "connect";
+                IP = tbDBIP.Text;
+                var dtTable = ofd.ExecQuery("select table_name from user_tables;");
+                List<string> temp = new List<string>();
+                foreach (DataRow dr in dtTable.Rows)
+                {
+                    temp.Add(dr[0].ToString());
+                }
+                allTable = temp;
+                this.Close();
+            }
+            else
+                Status = "connect fail";
+        }
+        
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OracleManagerStudio/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CommonFunction;
+
+namespace OracleManagerStudio
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Declarations
+        TreeViewItem currentTreeNodeParent = null;
+        Login lg = null;
+        #endregion
+
+        #region Property
+        public class dbInfo
+        {
+            public string dbName { get; set; }
+            public List<string> TableName { get; set; }
+            public string Status { get; set; }
+            public string IP { get; set; }
+            public string PW { get; set; }
+            public string ID { get; set; }
+        }
+
+        #endregion
+
+        #region MemberFunction
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtConnect_Click(object sender, RoutedEventArgs e)
+        {
+            lg = new Login();
+            lg.Show();
+            lg.Closed += Window_Closed;
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            dbInfo info = new dbInfo();
+            info.TableName = lg.allTable;
+            info.dbName = lg.dbName;
+            info.Status = lg.Status;
+            info.IP = lg.IP;
+            info.ID = lg.tbID.Text;
+            info.PW = lg.tbPW.Text;
+            TreeViewItem topNode = new TreeViewItem();
+            topNode.Header = info.dbName;
+            topNode.Tag = info;
+            foreach (var item in info.TableName)
+            {
+                TreeViewItem ti = new TreeViewItem();
+                ti.Header = item;
+                ti.MouseDoubleClick += TreeViewItem_MouseDoubleClick;
+                topNode.Items.Add(ti);
+                ti.Selected += Ti_Selected;
+            }
+            tree.Items.Add(topNode);
+        }
+        private void Ti_Selected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.OriginalSource as TreeViewItem;
+            if (item != null)
+            {
+                ItemsControl parent = GetSelectedTreeViewItemParent(item);
+                currentTreeNodeParent = parent as TreeViewItem;
+            }
+        }
+        public ItemsControl GetSelectedTreeViewItemParent(TreeViewItem item)
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(item);
+            while (!(parent is TreeViewItem || parent is TreeView))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            return parent as ItemsControl;
+        }
+        private void TreeViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if ((sender as TreeViewItem) is null)
+                return;
+            var dt = (sender as TreeViewItem).Header;
+            tbSQL.Text = "select * from " + dt;
+        }
+        private void TbSQL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F5)
+            {
+                ExecSQLQuery();
+            }
+        }
+        private void BtDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            tree.Items.Remove(currentTreeNodeParent);
+        }
+        #endregion
+
+        private void BtExecute_Click(object sender, RoutedEventArgs e)
+        {
+            ExecSQLQuery();
+        }
+
+        public void ExecSQLQuery()
+        {
+            var dt = (dbInfo)currentTreeNodeParent.Tag;
+            OraConnection ofd = new OraConnection(dt.IP, dt.dbName, dt.ID, dt.PW);
+            var dtofd = ofd.ExecQuery(tbSQL.Text);
+            dgResult.ItemsSource = dtofd.AsDataView();
+        }
+    }
+}
